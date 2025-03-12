37e448d66d866db4ffae82a603254184a9e9af48
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2025-03-12 21:43:27 +0100

    feat(exception): Add custom exceptions for user management

---
+++ back/src/main/java/com/openclassrooms/mddapi/exception/EmailAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.openclassrooms.mddapi.exception;
+
+public class EmailAlreadyExistsException extends RuntimeException {
+
+	public EmailAlreadyExistsException(String message) {
+		super(message);
+	}
+}
