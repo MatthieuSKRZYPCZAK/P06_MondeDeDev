8e38525bcb9e166cbc528c169be317ce596426c1
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2025-03-12 10:07:20 +0100

    feat: add .env and .env.example, update project dependencies and configuration
    - Added `.env` and `.env.example` for environment variable management.
    - Updated `pom.xml`:
      - Upgraded Spring Boot to version 3.4.3.
      - Added dependencies:
        - `spring-boot-starter-security`
        - `spring-boot-starter-validation`
        - `spring-boot-devtools`
        - `spring-boot-starter-oauth2-resource-server`
        - `modelmapper`
        - `dotenv-java`
        - `jjwt` (api, impl, jackson)
        - `lombok` (optional)
      - Changed MySQL driver to `mysql-connector-j`.
      - Excluded Lombok from the Spring Boot plugin.
    - Modified `MddApiApplication`:
      - Integrated `Dotenv` for environment variable loading.
      - Enabled `@EnableWebSecurity`.
    - Created and populated `application.properties` with database and encryption configurations.
    - Updated `.gitignore` to exclude `.env` from version control.

--- back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
+++ back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
@@ -1,12 +1,17 @@
 package com.openclassrooms.mddapi;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
+@EnableWebSecurity
 public class MddApiApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(MddApiApplication.class, args);
 	}
 
