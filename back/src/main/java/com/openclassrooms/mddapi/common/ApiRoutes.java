7cceba9b932415193cfd34d8236689e449e3143f
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2025-03-12 21:43:58 +0100

    Add: Define API route constants for authentication

---
+++ back/src/main/java/com/openclassrooms/mddapi/common/ApiRoutes.java
@@ -0,0 +1,17 @@
+package com.openclassrooms.mddapi.common;
+
+public class ApiRoutes {
+
+	public static final String BASE_URL = "/api";
+
+
+	/* Auth URL */
+	public static final String AUTH_URL = BASE_URL+"/auth";
+
+	public static final String LOGIN_URL = AUTH_URL+"/login";
+
+	public static final String REGISTER_URL = AUTH_URL+"/register";
+
+	public static final String ME_URL = AUTH_URL+"/me";
+
+}
