0b29ca8196bba2ed2145052200c8d4dc07245bb5
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2025-03-12 17:13:14 +0100

    fix: load environment variables in MddApiApplicationTests

--- back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
+++ back/src/main/java/com/openclassrooms/mddapi/MddApiApplication.java
@@ -3,10 +3,13 @@ package com.openclassrooms.mddapi;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 
 @SpringBootApplication
 @EnableWebSecurity
+@EnableJpaAuditing
 public class MddApiApplication {
 
 	public static void main(String[] args) {
@@ -14,5 +17,4 @@ public class MddApiApplication {
 		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(MddApiApplication.class, args);
 	}
-
 }
