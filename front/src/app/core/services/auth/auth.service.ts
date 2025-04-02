604526768718240821b0108fc5b137b8e84b2f77
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2025-04-02 19:14:07 +0200

    feat: implement AuthService with login, logout, token handling and user state

---
+++ front/src/app/core/services/auth/auth.service.ts
@@ -0,0 +1,62 @@
+import {Injectable, signal} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {LoginRequest} from '../../../features/auth/interfaces/loginRequest.interface';
+import {UserSessionInformation} from '../../../features/auth/interfaces/userSessionInformation.interface';
+import {Observable, tap} from 'rxjs';
+import {ApiRoutes} from '../../api/api-routes';
+import {User} from '../../../features/auth/interfaces/user.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  private LOGIN_URL = ApiRoutes.auth.login;
+  private ME_URL = ApiRoutes.auth.me;
+  private userSignal = signal<User | null>(null);
+
+  constructor(private http: HttpClient) {}
+
+  login(credentials: LoginRequest): Observable<UserSessionInformation> {
+    return this.http.post<UserSessionInformation>(this.LOGIN_URL, credentials)
+      .pipe(
+        tap(response => {
+          this.saveToken(response.token);
+          this.userSignal.set(response.user);
+        })
+      );
+  }
+
+  saveToken(token: string): void {
+    localStorage.setItem('token', token);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem('token');
+  }
+
+  getCurrentUser(): Observable<User> {
+    // return this.userSignal();
+    const token = this.getToken();
+    if(!token) {
+      throw new Error('No token found.');
+    }
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+
+    return this.http.get<User>(this.ME_URL, { headers })
+      .pipe(
+        tap(user => this.userSignal.set(user))
+      );
+  }
+
+  logout(): void {
+    localStorage.removeItem('token');
+    this.userSignal.set(null);
+  }
+
+  isAuthenticated(): boolean {
+    return !!this.getToken();
+  }
+}
