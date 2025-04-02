d684ad4f62ec1e6723692be225a789733f3eb4ed
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2025-04-02 19:14:56 +0200

    feat: add MessageService to display success, error and info snackbars

---
+++ front/src/app/core/services/message/message.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MessageService {
+
+  constructor(private snackBar: MatSnackBar) { }
+
+  showMessage(message: string, action: string = 'Fermer', duration: number = 3000, panelClass: string[] = []): void {
+    this.snackBar.open(message, action, {
+      duration,
+      panelClass,
+    });
+  }
+
+  showError(message: string): void {
+    this.showMessage(message, 'Fermer', 3000, ['snackbar-error']);
+  }
+
+  showSuccess(message: string): void {
+    this.showMessage(message, 'Fermer', 3000, ['snackbar-success']);
+  }
+
+  showInfo(message: string): void {
+    this.showMessage(message, 'Fermer', 3000, ['snackbar-info']);
+  }
+}
