69802b110f65b6df7ed20dbe4041a47a566ca193
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2025-04-02 19:13:37 +0200

    feat: define global message constants

---
+++ front/src/app/core/messages/messages.ts
@@ -0,0 +1,8 @@
+export const MESSAGES = {
+  AUTH: {
+    SERVICE_UNAVAILABLE: "Le service est actuellement indisponible",
+    INVALID_CREDENTIALS: "Identifiants invalides",
+    DEFAULT: "Une erreur est survenue"
+  },
+  LOGOUT_SUCCESS: "Déconnexion réussie"
+};
