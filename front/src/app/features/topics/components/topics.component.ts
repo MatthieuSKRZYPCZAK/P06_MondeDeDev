363dcfef261029a4ff4edb0945691b1cb90ba357
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2025-04-04 23:30:51 +0200

    feat(topics): implement dynamic topics listing with subscriptions handling

--- front/src/app/features/topics/components/topics.component.ts
+++ front/src/app/features/topics/components/topics.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {TopicService} from '../services/topic.service';
+import {AsyncPipe} from '@angular/common';
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
+import {AuthService} from '../../../core/services/auth/auth.service';
+
 
 @Component({
   selector: 'app-topics',
   standalone: true,
-  imports: [],
+  imports: [
+    AsyncPipe,
+    MatCardHeader,
+    MatCard,
+    MatCardContent,
+    MatCardTitle,
+  ],
   templateUrl: './topics.component.html',
   styleUrl: './topics.component.scss'
 })
+
 export class TopicsComponent {
+  private topicService = inject(TopicService);
+  private authService = inject(AuthService);
+
+  topics$ = this.topicService.getTopics();
+  userSignal = this.authService.getUser();
 
 }
