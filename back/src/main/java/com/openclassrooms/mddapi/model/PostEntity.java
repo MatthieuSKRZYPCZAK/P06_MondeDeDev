795f5f0a1e96748483d0329769e52e8afdffb545
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2025-04-06 21:25:28 +0200

    refactor(post): enforce non-null constraints and use TEXT type for post content

--- back/src/main/java/com/openclassrooms/mddapi/model/PostEntity.java
+++ back/src/main/java/com/openclassrooms/mddapi/model/PostEntity.java
@@ -17,7 +17,10 @@ public class PostEntity {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
+	@Column(nullable = false)
 	private String title;
+
+	@Column(columnDefinition = "TEXT", nullable = false)
 	private String content;
 
 	@ManyToOne(optional = false)
