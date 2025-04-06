c496bdef5e6e26fe963e43ab50a0d447be1b7e0f
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2025-04-06 21:24:33 +0200

    feat(profile): implement user profile form and subscription management in MeComponent

--- front/src/app/features/me/components/me.component.ts
+++ front/src/app/features/me/components/me.component.ts
@@ -1,14 +1,84 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {FormBuilder, FormGroup, ReactiveFormsModule} from '@angular/forms';
+import {Topic} from '../../topics/interfaces/topic.interface';
+import {Subject, takeUntil} from 'rxjs';
+import {TopicService} from '../../topics/services/topic.service';
+import {AuthService} from '../../../core/services/auth/auth.service';
+import {User} from '../../auth/interfaces/user.interface';
+import {MESSAGES} from '../../../core/messages/messages';
+import {MessageService} from '../../../core/services/message/message.service';
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
 
 
 @Component({
   selector: 'app-me',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardHeader,
+    MatCardContent,
+    MatCardHeader,
+    MatCardTitle,
+    ReactiveFormsModule
+  ],
   templateUrl: './me.component.html',
   styleUrl: './me.component.scss'
 })
-export class MeComponent{
+export class MeComponent implements OnDestroy {
+  profileForm: FormGroup;
+  user?: User;
+  topics: Topic[] = [];
+  private readonly destroy$ = new Subject<void>();
+
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private topicService: TopicService,
+    private messageService: MessageService,
+  ) {
+    this.profileForm = this.fb.group({
+      username: [''],
+      email: [''],
+      password: [''],
+    });
+
+    this.topicService.getTopics()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(topics => {
+        this.topics = topics;
+      });
+
+    this.authService.getCurrentUser()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe(user => {
+        this.user = user;
+        this.profileForm.patchValue({
+          username: user.username,
+          email: user.email,
+          password: '',
+        });
+      });
+  }
+
+  get userTopics(): Topic[] {
+    if (!this.user) return [];
+    return this.topics.filter(topic => this.user?.topics.includes(topic.name));
+  }
+
+  unsubscribe(topic: Topic) {
+    this.topicService.unsubscribeFromTopic(topic.name).subscribe((updatedUser: User) => {
+      this.authService.updateUser(updatedUser);
+      this.user = updatedUser;
+      this.messageService.showInfo(MESSAGES.UNSUBSCRIBE_SUCCESS(topic.label));
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}{
 
 
 }
